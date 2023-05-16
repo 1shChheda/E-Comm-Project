4b1fa837cdbfb2760fe1ba2bcc32bce0bfcea3c1
1shChheda <1shvenom786@gmail.com>
2023-05-16 22:23:56 +0530

    add product page, POST handler(+ redirection), body-parser middleware

--- app.js
+++ app.js
@@ -15,76 +15,57 @@
     //     console.log("Server is running at port 3000...");
     // });
 
+    
+// ----------------------------------------------------------------------
 
-// Working with Express (04-A)
-
-    // Writing code with NodeJS alone (i.e. with core modules/packages alone), the code is too long for even simple tasks like extracting data from the incoming request, etc
-
-    // to focus more on Bussiness Logic , & write a neater code --> we use other Packages/Frameworks ( like Express )
-
-    // Alternatives to Express.js
-
-        // Vanilla Node.js
-        // Adonis.js
-        // Koa
-        // Sails.js
+// Working with Express (04-B)
 
 const express = require('express');
 
-    // The `express` package exports a function
-    // When you require the express package using `const express = require('express');` , 
-        // you are essentially importing that exported function
-
 const app = express(); 
-    // By invoking that function (`express()`), 
-    // you create a new instance of the Express application, 
-    // which is assigned to the `app` variable
-    // The `app` variable represents the Express application object --> core of your web server
 
-// What Is MIDDLEWARE ?
+const bodyParser = require('body-parser');
 
-    // an Incoming Request is automatically funneled through a bunch of functions, until (in the end) you send a Response
+app.use(bodyParser.urlencoded()); // It returns a middleware like any other, plus it does the WHOLE BODY PARSING thing we did manually earlier (in routes.js) 
 
-    // you can use other third-party packages, which can give such middleware functions
+// app.use(bodyParser.urlencoded({extended: false})); // to avoid the safety alert text
 
-app.use((req, res, next) => { // 1) `Use` allows us to add a new 'middleware' function
-
-    // 2) this arrow-head function (inside the `app.use`) will be executed for every incoming request 
-
-    // 3) three arguments : `req` & `res` (like before) & `next`
-
-    // 4) `next` is itself a function, which needs to be executed, to allow the Request to travel on to the next middleware function
-
-    // 5) you either add a `next()` to send the request to next middleware, OR `send a Response`(to not do anything else)
-
-    // 6) if none of the two is mentioned, then its a "dying request"
-
-    if (req.url === '/favicon.ico') {
-        // Skip further processing for favicon request
-        return res.sendStatus(204);
-    }
-
-    console.log("in the middleware!"); // to see that when we go to host:3000, the Request from the url comes here, & inside this function & this line gets executed after that
-
-    next();
+app.use('/products', (req, res, next) => {
+    console.log("in the products page");
+    res.send(`
+        <h1>Products Page</h1>
+        <form action="/cart" method="POST">
+            <input type="text" name="product"autocomplete="off">
+            <button type="submit">Add To Cart</button>
+        </form>
+    `); 
+    // next(); // No Need. Will Result in an Error
 });
 
-app.use((req, res, next) => {
+// app.use('/cart', (req, res, next) => {
+//     console.log(req.body); // needs to be parsed separately (else it'll give `undefined`)
+//     res.redirect('/'); // much more convenient than conventional code used earlier
+// });
+
+// `app.get()`& `app.post()` are same as `app.use()` , but IT ONLY FIRES FOR AN INCOMING GET REQUEST & POST REQUEST respectively
+
+app.post('/cart', (req, res, next) => {
+    console.log(req.body); // needs to be parsed separately (else it'll give `undefined`)
+    res.redirect('/'); // much more convenient than conventional code used earlier
+});
+
+app.use('/', (req, res, next) => {
 
     if (req.url === '/favicon.ico') {
         // Skip further processing for favicon request
         return res.sendStatus(204);
     }
+
+    // '/' does not mean the full path (after the domain) has to be a `/`, but it has to start with that
 
     console.log("in the next middleware!");
-
     res.send(`<h1>Hello World! from NodeJS</h1>`); 
-        // `send` is allows us to send a Response of ANY TYPE! 
-        // (can be plain-text, HTML, JSON, Buffer, Status Code, etc...)
-        // it automatically sets the appropriate content-type header based on the data you provide
 });
-
-// NOTE : I've added " if (req.url === '/favicon.ico') {  return res.sendStatus(204); } " in the above middleware functions, so that THEY DO NOT EXECUTE for `favicon.ico Request` --> previously, console.logs were getting printed TWICE --> so I fixed the odd behaviour by ignoring `favicon.ico Request`
 
 app.listen(3000, () => {
     console.log("Server is running at port 3000...");
